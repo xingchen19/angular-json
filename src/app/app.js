a6887cd80005e5af58e3e3b119476f919878093b
francis <francis.li@163.com>
2017-10-27 07:53:47 -0400

    creat repo

---
+++ src/app/app.js
@@ -0,0 +1,44 @@
+/* use strict */
+import angular from 'angular';
+// import '../style/app.css';
+
+var app = angular.module("loadJsonApp", []);
+var jsonUrl = "../json/"
+
+app.service("loadJsonNameService",function ($http,$q){
+  var jsonNames = $q.defer();
+  $http.get(jsonUrl).then(function (jsonNameData) {
+    jsonNames.resolve(jsonNameData);
+  });
+  this.getplayers = function ()
+  {
+    return jsonNames.promise;
+  }
+})
+
+app.service("loadJsonService",function ($http, $q){
+  var deferred = $q.defer();
+  $http.get(jsonUrl + 'demo.json').then(function(data)
+  {
+    deferred.resolve(data);
+  });
+
+  this.getplayers =function ()
+  {
+    return deferred.promise;
+  }
+})
+
+.controller("loadJsonCtrl", function($scope, loadJsonNameService, loadJsonService){
+  var promise = loadJsonNameService.getplayers();
+  promise.then(function (data){
+    $scope.jsons = data.data;
+    console.log($scope.jsons);
+    var promise = loadJsonService.getplayers();
+    promise.then(function(data){
+      $scope.jsonData = data;
+      console.log($scope.jsonData);
+    });
+  });
+
+})
